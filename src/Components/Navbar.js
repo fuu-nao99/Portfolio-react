e29a97885dcfe875cf6686b79ca92c2fbd94f53f
nnvv <81692623+fuu-nao99@users.noreply.github.com>
2022-04-02 01:49:53 +0900

    デザイン修正

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,12 +7,12 @@ export default function Navbar() {
   const [navSize, setnavSize] = useState("6rem");
   const [navShadow, setnavShadow] = useState("none");
   const [navColor, setnavColor] = useState("transparent");
-  const [font, setFont] = useState("#8a8a8a");
-  const [fontColor, setfontColor] = useState("#F6F6F6");
+  const [font, setFont] = useState("#F6F6F6");
+  const [fontShadow, setfontShadow] = useState("#382a26");
   const listenScrollEvent = () => {
     window.scrollY > 10 ? setnavColor("#F6F6F6") : setnavColor("transparent");
-    window.scrollY > 10 ? setFont("#382a26") : setFont("#8a8a8a");
-    window.scrollY > 10 ? setfontColor("#8a8a8a") : setfontColor("#F6F6F6");
+    window.scrollY > 10 ? setFont("#382a26") : setFont("#F6F6F6");
+    window.scrollY > 10 ? setfontShadow("none") : setfontShadow("#382a26");
     window.scrollY > 10 ? setnavShadow("0px 1px 10px #999") : setnavShadow("none");
     window.scrollY > 10 ? setnavSize("4rem") : setnavSize("6rem");
 
@@ -26,11 +26,29 @@ export default function Navbar() {
   }, []);
 
   const hoverStyle = css`
+    filter: drop-shadow(0px 0px 5px ${fontShadow});
     color: ${font};
-    &:hover { 
-      color: ${fontColor};
-    }
     text-decoration : none;
+    position: relative;
+    &:hover {
+      color: ${font};
+    }
+    &::after {
+      position: absolute;
+      bottom: -4px;
+      left: 0;
+      content: '';
+      width: 100%;
+      height: 2px;
+      background: ${font};
+      transform: scale(0, 1);
+      transform-origin: center top;
+      transition: transform .3s;
+    }
+    &:hover::after { 
+      transform: scale(1, 1);
+    }
+
   `;
 
   return (
